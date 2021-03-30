5a148f89b1acdc38c1a479784eed4698da5ff3cf
llx1001 <llx771436886@163.com>
2021-03-30 18:28:42 +0800

    springcloud 修改

--- zuul-service-10010/src/main/java/com/huihex/zuul/ZuulServer10010.java
+++ zuul-service-10010/src/main/java/com/huihex/zuul/ZuulServer10010.java
@@ -3,11 +3,10 @@ package com.huihex.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableZuulProxy   // 开启网关功能
+@EnableZuulProxy        //开启网关功能
 @EnableDiscoveryClient  //开启eureka客户端
 public class ZuulServer10010 {
 
