d65eb654d19ca67883c283dd5dd4cf2fb42eff46
llx <llx771436886@163.com>
2021-04-11 09:47:20 +0800

    springcloud 修改44

--- zuul-service-10010/src/main/java/com/huihex/zuul/filter/Demo33.java
+++ zuul-service-10010/src/main/java/com/huihex/zuul/filter/Demo33.java
@@ -7,5 +7,6 @@ package com.huihex.zuul.filter;
 public class Demo33 {
     public static void main(String[] args) {
         System.out.println("厕所是是是");
+        System.out.println("厕所是是是");
     }
 }
